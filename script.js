420919d416e78a3fa12bdf4ac9ed5f497c7937a9
Siarhei Tsukanau <swtsukanov@gmail.com>
2021-02-11 18:41:46 +0300

    2 sections left

---
+++ script.js
@@ -0,0 +1,6 @@
+$('.slider').slick({
+    dots: false,
+    arrows: true,
+    nextArrow: '<button class="arrow" aria-hidden="true"><i class="fas fa-chevron-right"></i></button>',
+    prevArrow: '<button class="arrow" aria-hidden="true"><i class="fas fa-chevron-left"></i></button>'
+})
